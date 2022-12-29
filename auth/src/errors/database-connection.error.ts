d1e82cf80eba4c9237444ebd23883944274a0701
Michał Trocha <michal@dietly.pl>
2022-12-30 00:50:24 +0100

    auth - make error handler very generic

--- auth/src/errors/database-connection.error.ts
+++ auth/src/errors/database-connection.error.ts
@@ -5,7 +5,7 @@ export class DatabaseConnectionError extends CustomError {
   reason = 'Error connecting to database';
 
   constructor() {
-    super();
+    super('Error connecting to database');
 
     // Only because we are extending a build in class https://stackoverflow.com/a/41102306
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
