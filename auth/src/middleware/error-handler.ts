fd146f1c492e36c507da7790fd069f5b5a7cd909
Michał Trocha <michal@dietly.pl>
2022-12-30 00:18:48 +0100

    auth - custom error handlers (wip)

--- auth/src/middleware/error-handler.ts
+++ auth/src/middleware/error-handler.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection.error';
 
 export function errorHandler(
   err: Error,
@@ -6,9 +8,22 @@ export function errorHandler(
   res: Response,
   next: NextFunction
 ) {
-  console.log('Sth wrong:', err);
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map(error => ({
+      message: error.msg,
+      field: error.param,
+    }));
+
+    return res.status(400).send({ errors: formattedErrors });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    const formattedErrors = [{ message: err.reason }];
+
+    return res.status(500).send({ errors: formattedErrors });
+  }
 
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: 'Something went wrong' }],
   });
 }
