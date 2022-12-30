05c7e348c8a1468e6916a42b52f0f0ce3de91258
Michał Trocha <michal@dietly.pl>
2022-12-30 17:37:11 +0100

    auth - signup logic wip

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,14 +1,14 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection.error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
 router.post(
   '/api/users/signup',
   [
-    body('email').isEmail().withMessage('Email must be valid'),
+    body('email').isEmail().withMessage('Email must be sralid'),
     body('password')
       .trim()
       .isLength({ min: 4, max: 20 })
@@ -23,10 +23,17 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log('Creating a user... ');
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({ status: 'good' });
+    if (existingUser) {
+      console.log('email in use ');
+      return res.send({});
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
